5f295a9d1ad9ca9411640dc375e9a1b98218a977
Kwadwo Agyapon-Ntra <kayoghonline@gmail.com>
2023-05-13 14:49:22 +0200

    removed rate limit warning for demo

--- main.py
+++ main.py
@@ -204,12 +204,12 @@ with header_col:
     # Subtitle
     st.subheader("A cross-lingual semantic search tool built for PDF's")
 # Warning about rate-limiting
-st.write("---")
-with st.expander("⚠️ **Rate-limit note...**"):
-    st.info("This app uses Cohere's trial key, which is free, but has [usage limits](https://docs.cohere.com/docs/going-live#trial-key-limitations).  \n"\
-            """Effectively, you _**cannot**_ make multiple searches in one minute. If you encounter an error, wait about 30 seconds and try again.  \n\
-            Video walkthrough [here](https://youtu.be/GZTAFR0eeZo)""")
-st.write("---")
+# st.write("---")
+# with st.expander("⚠️ **Rate-limit note...**"):
+#     st.info("This app uses Cohere's trial key, which is free, but has [usage limits](https://docs.cohere.com/docs/going-live#trial-key-limitations).  \n"\
+#             """Effectively, you _**cannot**_ make multiple searches in one minute. If you encounter an error, wait about 30 seconds and try again.  \n\
+#             Video walkthrough [here](https://youtu.be/GZTAFR0eeZo)""")
+# st.write("---")
 
 # File uploader
 uploaded_files = st.file_uploader(
